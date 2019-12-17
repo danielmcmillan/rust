74169d79ead5b17d7bdbbcf4e6ec11e4d2331e5b
Daniel McMillan <daniel.mcmillan@ir.com>
2019-12-17 13:02:52 +1100

    minigrep parse_config fn

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,22 +3,34 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let string_args = env::args().collect::<Vec<String>>();
-    let str_args = string_args
+    let args = env::args().collect::<Vec<String>>();
+    match parse_config(&args) {
+        Ok(config) => do_stuff(&config),
+        Err(err) => println!("{}", err),
+    }
+}
+
+struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+fn parse_config(args: &[String]) -> Result<Config, String> {
+    let str_args = args
         .iter()
         .map(|x| x.as_str())
         .collect::<Vec<&str>>();
 
     match &str_args[..] {
-        [_, query, filename] => do_stuff(query, filename),
-        [exe, ..] => println!("Usage: {} pattern filename", exe),
-        _ => println!("Invalid arguments"),
+        [_, query, filename] => Ok(Config { query, filename }),
+        [exe, ..] => Err(format!("Usage: {} pattern filename", exe)),
+        _ => Err(String::from("Invalid arguments")),
     }
 }
 
-fn do_stuff(query: &str, filename: &str) {
-    println!("Searching for {} in {}", query, filename);
+fn do_stuff(config: &Config) {
+    println!("Searching for {} in {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Failed to read the file");
+    let contents = fs::read_to_string(config.filename).expect("Failed to read the file");
     println!("File contents:\n{}", contents);
 }
